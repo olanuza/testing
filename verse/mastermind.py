62ed78ac323ca45527cf96985e6dfe08cc89d7b6
uri2293 <oriol@lanuza.eu>
2019-03-19 19:53:01 +0100

    el server s'executa des del .py

--- verse/mastermind.py
+++ verse/mastermind.py
@@ -1,1 +1,18 @@
 # Mastermind Verse Test
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
