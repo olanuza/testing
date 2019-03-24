b5d1d87758a076f0d9490d99321ae04832f28b28
uri2293 <oriol@lanuza.eu>
2019-03-24 11:16:29 +0100

    now int2list return a list of int, not str. tested

--- ingenia/ex1.py
+++ ingenia/ex1.py
@@ -3,6 +3,8 @@
 # Parse integer to digit list
 def int2list(N):
     dlist = []
-    for digit in N:
-        dlist.append(digit)
+    for digit in str(N):
+        dlist.append(int(digit))
     return dlist
+
+print(int2list(12345))
