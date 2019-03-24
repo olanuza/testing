b651f41f43c59b0765e2444a5eea8f575df76238
uri2293 <oriol@lanuza.eu>
2019-03-24 10:59:01 +0100

    ex1 added

--- verse/mastermind.py
+++ verse/mastermind.py
@@ -1,10 +1,13 @@
 # Mastermind Verse Test
 
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('rate', type=int, help='Rate to charge for this resource')
+args = parser.parse_args()
 
 todos = {}
 
