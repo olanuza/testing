52c4c436de510832fbb198453b489cd9100fae0a
uri2293 <oriol@lanuza.eu>
2019-03-24 11:39:41 +0100

    minimal valencia function + test

--- ingenia/ex1.py
+++ ingenia/ex1.py
@@ -1,10 +1,16 @@
 # Exercise 1
 
-# Parse integer to digit list
-def int2list(N):
-    dlist = []
-    for digit in str(N):
-        dlist.append(int(digit))
-    return dlist
 
-print(int2list(12345))
+# Minimal version of valencia function. As we compare absolute number, sum order is not important
+def valencia_min(N):
+    odd = 0
+    even = 0
+    for position, digit in enumerate(str(N)):
+        if ((position%2)==0):
+            even += int(digit)
+        else:
+            odd += int(digit)
+    return((odd-even)==0)
+
+print("TRUE", valencia_min(111111))
+print("FALSE", valencia_min(1111111))
