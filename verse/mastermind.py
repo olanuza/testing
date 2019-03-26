9f1664001bc25f29b8baf9316bbf953ae577d1d3
uri2293 <oriol@lanuza.eu>
2019-03-26 22:04:10 +0100

    1st approach

--- verse/mastermind.py
+++ verse/mastermind.py
@@ -1,25 +1,18 @@
 # Mastermind Verse Test
 
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
-api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('rate', type=int, help='Rate to charge for this resource')
-args = parser.parse_args()
 
-todos = {}
+@app.route('/newgame/<string:id>/<string:code>')
+def new_game(id, code):
+    # if id exists, return error
+    # check if code is valid (len, colors)
+    # add new game to DB (id, code)
 
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
+@app.route('/guess/<string:id>/<string:guess>')
+def guess(id, guess):
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
