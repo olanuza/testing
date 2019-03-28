c2d7e463e4620b219659de39418c121149da108b
uri2293 <oriol@lanuza.eu>
2019-03-28 22:35:33 +0100

    a lot of simultaneous games

--- verse/mastermind.py
+++ verse/mastermind.py
@@ -1,18 +1,31 @@
-# Mastermind Verse Test
+# -*- coding: utf-8 -*-
+"""Mastermind Verse Test"""
 
-from flask import Flask, request
+__author__ = 'Oriol Lanuza Fisas'
+__email__ = 'oriol@lanuza.eu'
+__copyright__ = 'Copyright © 2019, Oriol Lanuza Fisas'
+__license__ = 'MIT'
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/newgame/<string:id>/<string:code>')
+@app.route('/game/<string:id>/<string:code>', methods=['POST'])
 def new_game(id, code):
     # if id exists, return error
     # check if code is valid (len, colors)
-    # add new game to DB (id, code)
+    # add new active game to DB (id, code, active)
+    return 0
 
-@app.route('/guess/<string:id>/<string:guess>')
+@app.route('/game/<string:id>/<string:guess>', methods=['POST'])   # we use post instead of get (we store info to db)
 def guess(id, guess):
-    
+    # if id exists, check active, if not, game does not exist
+    # if is active, make guess (create new turn + result with id game) and return result
+    return 0
 
+@app.route('/game/<string:id>', methods=['GET'])
+def historic(id):
+    # check if game exists and return historic of turn + guess + result
+    return 0
 if __name__ == '__main__':
     app.run(debug=True)
